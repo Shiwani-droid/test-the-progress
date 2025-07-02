d347bc789ba58a4210eaca762f2e0ef62a5ccf72
Shiwani-droid <77683897+Shiwani-droid@users.noreply.github.com>
2025-07-02 21:22:33 +0100

    Update add-Employee-Portal.spec.ts
    username password updated

--- tests/add-Employee-Portal.spec.ts
+++ tests/add-Employee-Portal.spec.ts
@@ -7,8 +7,8 @@ import { employeeDeatils } from './fixture/testData'
 
 test('Add Employee to the BrightHR portal', async ({ page }) => {
   await page.goto('/login');
-  await page.locator('#username').fill('shiwanisandal@gmail.com');
-  await page.locator('#password').fill('Password@123')
+  await page.locator('#username').fill('qaAutomationTechTask@grr.la');
+  await page.locator('#password').fill('A1234567890-')
   await page.locator('button', { hasText: 'Login' }).click();
   await page.waitForURL('**/dashboard');
   await expect(page.locator('text="SB"')).toBeVisible();
